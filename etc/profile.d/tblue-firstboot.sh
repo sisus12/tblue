1222fbad18779bfe31de1ff3a3a064399419147d
sisus12 <135336609+sisus12@users.noreply.github.com>
2023-06-02 20:54:35 +0200

    Update tblue-firstboot.sh

--- etc/profile.d/tblue-firstboot.sh
+++ etc/profile.d/tblue-firstboot.sh
@@ -1,5 +1,5 @@
 if test "$(id -u)" -gt "0" && test -d "$HOME"; then
-    if test ! -e "$HOME"/.config/autostart/tblue-firstboot.desktop; then
+    if test ! -e "$HOME"/.config/tblue/firstboot-done; then
         mkdir -p "$HOME"/.config/autostart
         cp -f /etc/skel.d/.config/autostart/tblue-firstboot.desktop "$HOME"/.config/autostart
     fi
